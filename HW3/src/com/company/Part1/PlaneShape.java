f7d3da77994d22922089b7f7537d569796deddce
bag9l <76074096+bag9l@users.noreply.github.com>
2021-04-04 13:38:54 +0300

    HW

--- HW3/src/com/company/Part1/PlaneShape.java
+++ HW3/src/com/company/Part1/PlaneShape.java
@@ -3,15 +3,6 @@ package com.company.Part1;
 import com.company.Part1.Interfaces.AreaMeasurable;
 import com.company.Part1.Interfaces.PerimeterMeasurable;
 
-public class PlaneShape extends Shape implements PerimeterMeasurable, AreaMeasurable {
+public abstract class PlaneShape extends Shape implements PerimeterMeasurable, AreaMeasurable {
 
-
-    public float getPerimeter() {
-        return 0;
-    }
-
-
-    public float getArea() {
-        return 0;
-    }
 }
--- HW3/src/com/company/Part1/SpaceShape.java
+++ HW3/src/com/company/Part1/SpaceShape.java
@@ -3,14 +3,6 @@ package com.company.Part1;
 import com.company.Part1.Interfaces.AreaMeasurable;
 import com.company.Part1.Interfaces.VolumeMeasurable;
 
-public class SpaceShape extends Shape implements AreaMeasurable, VolumeMeasurable {
+public abstract class SpaceShape extends Shape implements AreaMeasurable, VolumeMeasurable {
 
-    public float getArea() {
-        return 0;
-    }
-
-
-    public double getVolume() {
-        return 0;
-    }
 }
