e2507e9b274770ec40ae432763bcaabe69883247
BohdanKarp <81150034+BohdanKarp@users.noreply.github.com>
2021-04-19 10:49:04 +0300

    Product

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,85 @@
 package main.java.model;
 
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Product {
+    private static final AtomicInteger count = new AtomicInteger(0);
+    int id;
+    String name;
+    double price;
+    int amountInStock;
+    ProductCategory category;
+
+    public Product(String name, double price, int amountInStock, ProductCategory category) {
+        this.id = count.incrementAndGet();
+        this.name = name;
+        this.price = price;
+        this.amountInStock = amountInStock;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAmountInStock() {
+        return amountInStock;
+    }
+
+    public void setAmountInStock(int amountInStock) {
+        this.amountInStock = amountInStock;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                ", amountInStock=" + amountInStock +
+                ", category=" + category +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product that = (Product) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
---
+++ src/main/java/model/ProductCategory.java
@@ -0,0 +1,5 @@
+package main.java.model;
+
+public enum ProductCategory {
+    FOOD, SOFT_DRINKS, ALCOHOL, CHEMISTRY, OTHER, BUILDING_MATERIALS, MACHINRRY
+}
